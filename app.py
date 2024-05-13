717254af2fcd39750354b0b3dcab394e513578a5
PagP332 <120449335+PagP332@users.noreply.github.com>
2024-05-14 01:23:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,13 +25,13 @@ def import_and_predict(image_data, model):
     
         size = (150,150)    
         image = ImageOps.fit(image_data, size)
-        image = np.asarray(image)
+        img = np.asarray(image)
         #img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
         
-        #img_reshape = img[np.newaxis,...]
+        img_reshape = img[np.newaxis,...]
     
-        prediction = model.predict(image)
+        prediction = model.predict(img_reshape)
         
         return prediction
 if file is None:
