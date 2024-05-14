f1552e44efbc9a68a57215e5bb8d0e20f547654b
PagP332 <120449335+PagP332@users.noreply.github.com>
2024-05-14 20:12:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,6 @@ import streamlit as st
 def load_model():
   model=tf.keras.models.load_model('deploy_cnn.best.hdf5')
   return model
-with st.spinner('Model is being loaded..'):
-  model=load_model()
-
-st.write("""
-         # Intel Image Classification
-         """
-         )
-
-file = st.file_uploader("Please upload a 150x150 image file (PNG or JPG only)", type=["jpg", "png"])
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-st.set_option('deprecation.showfileUploaderEncoding', False)
 def import_and_predict(image_data, model):
     
         size = (150,150)    
@@ -34,13 +21,26 @@ def import_and_predict(image_data, model):
         prediction = model.predict(img_reshape)
         
         return prediction
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Intel Image Classification
+         """
+         )
+
+file = st.file_uploader("Please upload a 150x150 image file (PNG or JPG only)", type=["jpg", "png"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    #predictions = import_and_predict(image, model)
-    prediction = model.predict(image)
+    predictions = import_and_predict(image, model)
+    #prediction = model.predict(image)
     score = tf.nn.softmax(predictions[0])
     st.write(prediction)
     st.write(score)
